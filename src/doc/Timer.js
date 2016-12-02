ea1804432934711424a65df1da52ed487b74cae6
wangboy <bjutwangboy@gmail.com>
2016-12-02 11:33:07 +0800

    form and select

--- src/doc/Timer.js
+++ src/doc/Timer.js
@@ -10,12 +10,16 @@ class Timer extends React.Component {
     this.state = {
       seconds: 0,
       resetTimes: 0,
-      text: ''
+      text: '',
+      sel: '10'
     };
 
     this.reset = this.reset.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleTextChange = this.handleTextChange.bind(this)
+
+    this.handleSelectChange = this.handleSelectChange.bind(this)
+    this.handleSubmitSelect = this.handleSubmitSelect.bind(this)
   }
 
   tick() {
@@ -49,9 +53,21 @@ class Timer extends React.Component {
   }
 
   handleTextChange(e) {
-    this.setState({text: e.target.value})
-    console.log(' e.target is ' + e.target) //HTMLInputElement
-    e.target.focus()
+    let input = e.target.value;
+    if (!isNaN(parseInt(input, 10))) { //parseInt '1122sss' = 1122
+      this.setState({text: e.target.value})
+      console.log(' e.target is ' + e.target) //HTMLInputElement
+      e.target.focus()
+    }
+  }
+
+  handleSubmitSelect(e) {
+    e.preventDefault()
+    this.setState({seconds: parseInt(this.state.sel, 10)})
+  }
+
+  handleSelectChange(e) {
+    this.setState({sel: e.target.value})
   }
 
   render() {
@@ -59,6 +75,12 @@ class Timer extends React.Component {
     if (this.state.seconds % 2 !== 0) {
       even = <p>Odd</p>
     }
+
+    let options = []
+    for (let i = 10; i < 50; i += 10) {
+      options.push(<option value={i + ""}>{i + ""} sec</option>)
+    }
+
     return (
       <div className="Timer">
         <p onClick={this.reset}>Seconds : {this.state.seconds}</p>
@@ -71,7 +93,17 @@ class Timer extends React.Component {
             Enter a Number:
             <input type="text" ref={it => this._text = it} value={this.state.text} onChange={this.handleTextChange}/>
           </label>
-          <input type="submit" value="Submit"/>
+          <input type="submit" value="subInput"/>
+        </form>
+
+        <form onSubmit={this.handleSubmitSelect}>
+          <label>
+            Choose a second :
+            <select value={this.state.sel} onChange={this.handleSelectChange}>
+              {options}
+            </select>
+          </label>
+          <input type="submit" value="subSel"/>
         </form>
       </div>
     )
